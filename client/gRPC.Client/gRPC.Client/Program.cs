7d0ad03d53472c7810ef88a2f19fe29ebbe78be7
satyampushkar <42037502+satyampushkar@users.noreply.github.com>
2021-05-16 22:12:27 +0530

    grpc server and client side code with unary, server streaming and client-server streaming sample code

---
+++ client/gRPC.Client/gRPC.Client/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Google.Protobuf.WellKnownTypes;
+using gRPC.net.demo.stockDetails;
+using System.Threading;
+using Grpc.Core;
+
+namespace gRPC.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new StockService.StockServiceClient(channel);
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("******************************************");
+                Console.WriteLine("Press 1 for GetStockListings");
+                Console.WriteLine("Press 2 for GetStockPrice");
+                Console.WriteLine("Press 3 for GetStockPriceStream");
+                Console.WriteLine("Press 4 for GetCompanyStockPriceStream");
+                Console.WriteLine("Press 5 for Exit from app");
+                Console.WriteLine("******************************************");
+                Console.ResetColor();
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        GetStockListings(client);
+                        break;
+                    case "2":
+                        GetStockPrice(client);
+                        break;
+                    case "3":
+                        await GetStockPriceStream(client);
+                        break;
+                    case "4":
+                        await GetCompanyStockPriceStream(client);
+                        break;
+                    case "5":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private static async Task GetCompanyStockPriceStream(StockService.StockServiceClient client)
+        {
+            //To get all stock Ids
+            //var stocks = client.GetStockListings(new Empty()).Stocks; 
+            using var streamingCall = client.GetCompanyStockPriceStream();
+
+            // background task which uses async streams to read each stockPrice from the response steam.
+            _ = Task.Run(async () =>
+            {
+                try
+                {
+                    await foreach (var stockPrice in streamingCall.ResponseStream.ReadAllAsync())
+                    {
+                        Console.WriteLine(string.Format($"{stockPrice.Stock.StockId}\t {stockPrice.Stock.StockName}\t {stockPrice.Price}\t {stockPrice.DateTimeStamp}"));
+                    }
+                }
+                catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
+                {
+                    Console.WriteLine("Stream cancelled.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error reading response: " + ex);
+                }
+            });
+
+            //send requests through requst stream
+            foreach (var stockId in new[] { "FB", "AAPL", "AMZN", "MSFT", "GOOG" })
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"Requesting details for {stockId}...");
+                Console.ResetColor();
+
+                await streamingCall.RequestStream.WriteAsync(new Stock { StockId = stockId });
+
+                //Mimicing delay in sending request
+                await Task.Delay(1500);
+            }
+
+            Console.WriteLine("Completing request stream");
+            await streamingCall.RequestStream.CompleteAsync();
+            Console.WriteLine("Request stream completed");
+        }
+
+        private static async Task GetStockPriceStream(StockService.StockServiceClient client)
+        {
+            var cts = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+
+            using var streamingCall = client.GetStockPriceStream(new Empty(), cancellationToken: cts.Token);
+            try
+            {
+                Console.WriteLine(string.Format($"Stock Id\t Stock Name\t Stock Price\t TimeStamp"));
+                await foreach (var stockPrice in streamingCall.ResponseStream.ReadAllAsync(cancellationToken: cts.Token))
+                {
+                    Console.WriteLine(string.Format($"{stockPrice.Stock.StockId}\t {stockPrice.Stock.StockName}\t {stockPrice.Price}\t {stockPrice.DateTimeStamp}"));
+                }
+            }
+            catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
+            {
+                Console.WriteLine("Stream cancelled.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error reading response: " + ex);
+            }
+        }
+
+        private static void GetStockPrice(StockService.StockServiceClient client)
+        {
+            var result = client.GetStockPrice(new Stock { StockId = "FB" });
+            Console.WriteLine($"Stock Id: {result.Stock.StockId}\n Stock Name: {result.Stock.StockName}\n " +
+                $"Stock Price: {result.Price}\n TimeStamp: {result.DateTimeStamp}");
+        }
+
+        private static StockListing GetStockListings(StockService.StockServiceClient client)
+        {
+            var result = client.GetStockListings(new Empty());
+            foreach (var stock in result.Stocks)
+            {
+                Console.WriteLine($"{stock.StockId}\t{stock.StockName}");
+            }
+
+            return result;
+        }
+    }
+}
---
+++ server/gRPC.Server/gRPC.Server/Services/StockDataService.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using gRPC.net.demo.stockDetails;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace gRPC.Server.Services
+{
+    public class StockDataService : StockService.StockServiceBase
+    {
+        private readonly ILogger<StockDataService> _logger;
+
+        private static readonly List<Stock> _stocks = new List<Stock> 
+        {
+            {new Stock {StockId = "FB", StockName = "Facebook"} },
+            {new Stock {StockId = "AAPL", StockName = "Apple"} },
+            {new Stock {StockId = "AMZN", StockName = "Amazon"} },
+            {new Stock {StockId = "NFLX", StockName = "Netflix"} },
+            {new Stock {StockId = "MSFT", StockName = "Microsoft"} },
+            {new Stock {StockId = "TSLA", StockName = "Tesla"} },
+            {new Stock {StockId = "GOOG", StockName = "Alphabet"} }
+        }; 
+
+        public StockDataService(ILogger<StockDataService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<StockListing> GetStockListings(Empty request, ServerCallContext context)
+        {
+            return Task.FromResult(new StockListing { Stocks = { _stocks }  });
+        }
+
+        public override Task<StockPrice> GetStockPrice(Stock request, ServerCallContext context)
+        {
+            var rnd = new Random(100);
+            return Task.FromResult(
+                new StockPrice
+                {
+                    Stock = _stocks.FirstOrDefault(x => x.StockId == request.StockId),
+                    DateTimeStamp = DateTime.UtcNow.ToTimestamp(),
+                    Price = rnd.Next(100, 500).ToString()
+                });
+        }
+
+        public override async Task GetStockPriceStream(Empty request, IServerStreamWriter<StockPrice> responseStream, ServerCallContext context)
+        {            
+            int i = 10;
+            var rnd = new Random(100);
+            while (!context.CancellationToken.IsCancellationRequested && i > 0)
+            {
+                _stocks.ForEach(async s =>
+                {
+                    var time = DateTime.UtcNow.ToTimestamp();                    
+                    await responseStream.WriteAsync(new StockPrice 
+                    { 
+                        Stock = s,
+                        DateTimeStamp = time,
+                        Price = rnd.Next(100, 500).ToString()
+                    });
+                });
+
+                await Task.Delay(500);
+            }
+        }
+
+        public override async Task GetCompanyStockPriceStream(IAsyncStreamReader<Stock> requestStream, IServerStreamWriter<StockPrice> responseStream, ServerCallContext context)
+        {
+            // we'll use a channel here to handle in-process 'messages' concurrently being written to and read from the channel.
+            var channel = Channel.CreateUnbounded<StockPrice>();
+
+            // background task which uses async streams to write each stockPrice from the channel to the response steam.
+            _ = Task.Run(async () =>
+            {
+                await foreach (var stockPrice in channel.Reader.ReadAllAsync())
+                {
+                    await responseStream.WriteAsync(stockPrice);
+                }
+            });
+
+            // a list of tasks handling requests concurrently
+            var getCompanyStockPriceStreamRequestTasks = new List<Task>();
+
+            try
+            {
+                // async streams used to read and process each request from the stream as they are receieved
+                await foreach (var request in requestStream.ReadAllAsync())
+                {
+                    _logger.LogInformation($"Getting stock Price for {request.StockName}({request.StockId})");
+                    // start and add the request handling task
+                    getCompanyStockPriceStreamRequestTasks.Add(GetStockPriceAsync(request));
+                }
+
+                _logger.LogInformation("Client finished streaming");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An exception occurred");
+            }
+
+            // wait for all responses to be written to the channel 
+            // from the concurrent tasks handling each request
+            await Task.WhenAll(getCompanyStockPriceStreamRequestTasks);
+
+            channel.Writer.TryComplete();
+
+            //  wait for all responses to be read from the channel and streamed as responses
+            await channel.Reader.Completion;
+
+            _logger.LogInformation("Completed response streaming");
+
+            // a local function which defines a task to handle a Company Stock Price request
+            // it mimics 10 consecutive stock price, simulating a delay of 0.5s
+            // multiple instances of this will run concurrently for each recieved request
+            async Task GetStockPriceAsync(Stock stock)
+            {
+                var rnd = new Random(100);
+                for (int i = 0; i < 10; i++)
+                {
+                    var time = DateTime.UtcNow.ToTimestamp();
+                    await channel.Writer.WriteAsync(new StockPrice 
+                    {
+                        Stock = _stocks.FirstOrDefault(x => x.StockId == stock.StockId),
+                        Price = rnd.Next(100, 500).ToString(),
+                        DateTimeStamp = time
+                    });
+
+                    await Task.Delay(500);
+                }
+            }
+        }
+    }
+}
